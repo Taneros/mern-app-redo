afe63c250981f50dcccaf1de1a0faa6d2fbdad65
taneros <taneros@duck.com>
2024-01-14 23:18:06 +0300

    fix: db

---
+++ server/database/db.js
@@ -0,0 +1,15 @@
+const mongoose = require( "mongoose" );
+
+const connectToDB = async () => {
+    try {
+	const connect = await mongoose.connect(process.env.MONGO_URI, {
+	        dbName: process.env.DB_NAME,
+	    });
+	    console.log(`MongoDB connected: ${connect.connection.host}`);
+} catch (error) {
+	        console.error(`MongoDB connection error: ${error.message}`);
+
+}
+};
+
+module.exports = connectToDB;
